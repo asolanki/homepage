5cdb082200d89af0e6beea76c71db7311be4e05b
Adarsh Solanki <adarsh.solanki@gmail.com>
2023-11-10 18:32:54 +0800

    try waveform with whisper

--- js/tone-trainer.js
+++ js/tone-trainer.js
@@ -7,17 +7,26 @@ const status = document.getElementById("status");
 const recordButton = document.getElementById('record-button');
 const labelsContainer = document.getElementById('labels-container');
 
+const visualizerContainer = document.getElementById('visualizer-container');
+const audioVisualizer = document.getElementById('audioVisualizer');
+
+
 // load model
 status.textContent = "Loading model...";
-const classifier = await pipeline("audio-classification", "asolanki/distilhubert-finetuned-mandarin");
+const classifier = await pipeline("automatic-speech-recognition", "Xenova/whisper-tiny");
 status.textContent = "Ready";
 
 let mediaRecorder;
 let audioChunks = [];
 
+let mediaRecorder;
+let audioChunks = [];
+let audioContext, source, analyser;
+
 // Start recording when the spacebar is held down
 document.addEventListener('keydown', async function (event) {
-    if (event.code === 'Space') {
+    if (event.code === 'Space' && (!mediaRecorder || mediaRecorder.state === 'inactive')) {
+        visualizerContainer.style.background = '#d1f4d1'; // Start recording UI feedback
         const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
         audioChunks = [];
         mediaRecorder = new MediaRecorder(stream);
@@ -25,6 +34,13 @@ document.addEventListener('keydown', async function (event) {
         mediaRecorder.ondataavailable = event => {
             audioChunks.push(event.data);
         };
+
+        // Prepare for waveform visualization
+        audioContext = new AudioContext();
+        source = audioContext.createMediaStreamSource(stream);
+        analyser = audioContext.createAnalyser();
+        source.connect(analyser);
+        drawWaveform();
 
         mediaRecorder.start();
     }
@@ -34,6 +50,7 @@ document.addEventListener('keydown', async function (event) {
 document.addEventListener('keyup', async function (event) {
     if (event.code === 'Space' && mediaRecorder && mediaRecorder.state === 'recording') {
         mediaRecorder.stop();
+        visualizerContainer.style.background = '#f2d1d1'; // Processing UI feedback
 
         const audioBlob = new Blob(audioChunks, { type: 'audio/wav' });
         const audioBuffer = await blobToArrayBuffer(audioBlob);
@@ -41,27 +58,68 @@ document.addEventListener('keyup', async function (event) {
         // Model inference
         status.textContent = 'Analysing...';
         const output = await classifier(audioBuffer, {
-            // Any model-specific options here
+            // Model-specific options
         });
         status.textContent = '';
+
+        // Update UI based on results
+        // (Modify this part according to your actual output processing logic)
+        visualizerContainer.style.background = output.length > 0 ? '#c8e6c9' : '#ffcdd2'; // Success or error color
 
         // Display top 3 labels
         const topLabels = output.slice(0, 3); // Assuming output is sorted
         labelsContainer.innerHTML = '';
         topLabels.forEach(label => {
             const labelElement = document.createElement('h1');
-            labelElement.textContent = label.name; // Replace with the actual property for the label name
+            labelElement.textContent = label.name; // Replace with actual label property
             labelsContainer.appendChild(labelElement);
         });
     }
 });
 
-// Helper function to convert Blob to ArrayBuffer
+// Helper functions
 function blobToArrayBuffer(blob) {
-    return new Promise((resolve, reject) => {
-        const reader = new FileReader();
-        reader.onloadend = () => resolve(reader.result);
-        reader.onerror = reject;
-        reader.readAsArrayBuffer(blob);
-    });
+    // ... (Your existing function)
+}
+
+function drawWaveform() {
+    const canvas = audioVisualizer;
+    const ctx = canvas.getContext('2d');
+    const bufferLength = analyser.frequencyBinCount;
+    const dataArray = new Uint8Array(bufferLength);
+    const WIDTH = canvas.width;
+    const HEIGHT = canvas.height;
+
+    function draw() {
+        requestAnimationFrame(draw);
+
+        analyser.getByteTimeDomainData(dataArray);
+
+        ctx.fillStyle = '#f2f2f2';
+        ctx.fillRect(0, 0, WIDTH, HEIGHT);
+        ctx.lineWidth = 2;
+        ctx.strokeStyle = '#031d44';
+
+        ctx.beginPath();
+        let sliceWidth = WIDTH * 1.0 / bufferLength;
+        let x = 0;
+
+        for(let i = 0; i < bufferLength; i++) {
+            let v = dataArray[i] / 128.0;
+            let y = v * HEIGHT/2;
+
+            if(i === 0) {
+                ctx.moveTo(x, y);
+            } else {
+                ctx.lineTo(x, y);
+            }
+
+            x += sliceWidth;
+        }
+
+        ctx.lineTo(canvas.width, canvas.height/2);
+        ctx.stroke();
+    }
+
+    draw();
 }
