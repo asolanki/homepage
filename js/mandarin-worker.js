b756bc6e3a7e6826e364f904a962eab9ac4b7746
Adarsh Solanki <adarsh.solanki@gmail.com>
2024-02-16 00:48:29 +0700

    service worker scope

--- js/mandarin-worker.js
+++ js/mandarin-worker.js
@@ -15,15 +15,24 @@ self.addEventListener('install', function(event) {
 });
 
 self.addEventListener('fetch', function(event) {
-  event.respondWith(
-    caches.match(event.request)
-      .then(function(response) {
-        // Cache hit - return response
-        if (response) {
-          return response;
-        }
-        return fetch(event.request);
-      }
-    )
-  );
-});
+    event.respondWith(
+      caches.match(event.request)
+        .then(function(response) {
+          // Cache hit - return response
+          if (response) {
+            return response;
+          }
+  
+          // IMPORTANT: Clone the response. A response is a stream and because we want the browser to consume the response
+          // as well as the cache consuming the response, we need to clone it so we have two streams.
+          return fetch(event.request).then(response => {
+            let responseClone = response.clone();
+            caches.open(CACHE_NAME).then(cache => {
+              cache.put(event.request, responseClone);
+            });
+  
+            return response;
+          });
+        })
+    );
+  });
